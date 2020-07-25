172b582defda1a545f608bada257eb1f864d8b36
Sadia Batool <sadia110@my.yorku.ca>
2020-07-25 15:05:30 -0400

    edit handlebars functions

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 
 var express = require("express");
-var PORT = process.env.PORT || 3306;
-var app = express();
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
+var app = express();
+var PORT = process.env.PORT || 3000;
 // Serve static content 
 app.use(express.static("public"));
 
@@ -11,12 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //  Handlebars.
-var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Access for server to routes 
-var routes = require("./controllers/burgers_controllers.js");
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
