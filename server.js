7622ce9d14c9830f63b69da1ec6bb9790e8cdafb
Sadia Batool <sadia110@my.yorku.ca>
2020-07-25 03:34:05 -0400

    final

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 
-// Dependencies
-const express = require("express");
-const exphbs = require("express-handlebars");
 
-// Create an instance of the express app.
+const express = require("express");
+const exphbs = require("express-handlebars"); 
+
 const app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
-// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars")
