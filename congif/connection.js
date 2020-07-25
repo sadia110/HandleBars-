9ba663a5648be39b14ae4f0c3400589244a0d0ea
Sadia Batool <sadia110@my.yorku.ca>
2020-07-25 02:26:20 -0400

    created connection

--- congif/connection.js
+++ congif/connection.js
@@ -1,1 +1,25 @@
-var mysql = require("mysql");
+
+// require("dotenv").config();
+
+var mysql = require("mysql"); 
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Batool12",
+    database: "burger_db",
+    insecureAuth : true 
+
+
+  });
+}   
+
+connection.connect();
+module.exports = connection;
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Canadian");
+INSERT INTO burgers (burger_name) VALUES ("BBQ chicken");
+INSERT INTO burgers (burger_name) VALUES (" Veggie");
+INSERT INTO burgers (burger_name) VALUES ("Cheese");
